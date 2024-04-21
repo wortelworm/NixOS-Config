28c073d39953ed9434e39106a3b21add855906d3
wortelworm <wortelworm@gmail.com>
2024-04-21 12:41:50 +0200

    Trying out sddm wayland - wortelworm5 6

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -6,7 +6,11 @@
 
   # KDE Plasma Desktop Environment.
   # services.xserver.displayManager.sddm.enable = true;
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    # Altough expirimental, scaling seems to work better in sddm wayland
+    wayland.enable = true;
+  };
   services.xserver.desktopManager.plasma5.enable = true;
 
   # todo: hyprland, wayfire and sway?
