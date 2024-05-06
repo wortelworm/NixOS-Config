3f4d49effcc650c02e1a3ca446173b875fd1b3b7
wortelworm <wortelworm@gmail.com>
2024-05-06 11:02:19 +0200

    switched to plasma 6 - wortelworm5 28

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -5,17 +5,20 @@
   services.xserver.enable = true;
 
   # KDE Plasma Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
   services.displayManager.sddm = {
     enable = true;
     # custom scaling, '-nolisten tcp' is default
     settings.X11.ServerArguments="-nolisten tcp -dpi 200";
   };
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # maybe make the hyprland work properly and
   # test wayfire and sway as well one day...
   imports = [
     ./hyprland.nix
   ];
+
+  # previously enabled plasma 5 by doing:
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
 }
