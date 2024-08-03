4512bb099213e899612960d4d253796518ca03ec
wortelworm <wortelworm@gmail.com>
2024-08-03 12:14:33 +0200

    Removed plymouth again - wortelworm5 63

--- modules/nixos/grub.nix
+++ modules/nixos/grub.nix
@@ -3,6 +3,8 @@
 {
   # Bootloader.
   boot.loader = {
+    timeout = 5;
+
     efi = {
       efiSysMountPoint = "/boot";
       canTouchEfiVariables = true;
@@ -22,40 +24,7 @@
       icon = "color";
       screen = "4k";
     };
-
-    # Part of 'Silent boot'
-    # It's still possible to open the bootloader list by pressing any key
-    # It will just not appear on screen unless a key is pressed
-    timeout = 0;
   };
-
-  # Display eye candy while computer is starting
-  boot = {
-    plymouth = {
-      enable = true;
-      theme = "infinite_seal";
-      themePackages = with pkgs; [
-        # By default we would install all themes
-        (adi1090x-plymouth-themes.override {
-          selected_themes = [ "infinite_seal" ];
-        })
-      ];
-    };
-
-    # Enable "Silent Boot"
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-    ];
-  };
-
 
   # minegrub, here to stay as an example
   # boot.loader.grub.theme = pkgs.stdenv.mkDerivation rec {
