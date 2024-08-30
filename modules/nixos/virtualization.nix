b0658b2845925b80ac5502a3961e261511e27f65
wortelworm <wortelworm@gmail.com>
2024-08-30 12:33:02 +0200

    Testing dns redirect service - wortelworm5 82

--- modules/nixos/virtualization.nix
+++ modules/nixos/virtualization.nix
@@ -1,8 +1,17 @@
 { pkgs, ... }:
 
 {
-  # See also: https://wiki.nixos.org/wiki/Virt-manager
-  # Try to make windows vm
+  # Notes on virt-manager:
+  #   see also: https://wiki.nixos.org/wiki/Virt-manager
+  #   Enable more graphs: preferences -> polling
+  #   Fix screen: view -> scale display -> autoresize vm to window
+  #   Guest only using 1 cpu core: change cpu topology to one socket, more of the other stuff
+  #   Screen tearing: use video QXL instead of virtio
+  #   
+  # Notes for windows vm:
+  #   Install winfsp and virtio-win-guest-tools on guest for folder sharing
+  #   Visual studio installer: Select 'windows application environment', not '.NET desktop environment'
+  #   
   programs.virt-manager.enable = true;
   virtualisation.libvirtd = {
     enable = true;
@@ -23,5 +32,17 @@
     };
 
     # Daemon settings?
+  };
+  
+  # Redirect local dns port to point at pi-hole
+  systemd.user.services.wortel-dns-redirect = {
+    enable = true;
+    after = [ "network.target" ];
+    wantedBy = [ "default.target" ];
+    description = "Wortel dns port redirection";
+    serviceConfig = {
+        Type = "simple";
+        ExecStart = ''iptables -t nat -A OUTPUT -d 127.0.0.1 -p udp --dport 53 -j REDIRECT --to-ports 5353'';
+    };
   };
 }
