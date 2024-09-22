41ab8446ba52b3ea8d9504fe65e650d3960dd943
wortelworm <wortelworm@gmail.com>
2024-09-22 18:06:57 +0200

    updates - wortelworm5 94

--- modules/home-manager/programs/nixvim/cp-template.cpp
+++ modules/home-manager/programs/nixvim/cp-template.cpp
@@ -8,6 +8,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+#define all(x) (x).begin(), (x).end()
+#define loop(X, N) for(int X 0; X < (N); X++)
+
 int main() {
     // freopen("./", "r", stdin);
 
