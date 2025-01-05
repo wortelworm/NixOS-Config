ebacb844e67ba84f3d7e86cde4238d12b6bce489
wortelworm <wortelworm@gmail.com>
2025-01-05 13:56:51 +0100

    XDG home directories changed attempt - wortelworm5 144

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -1,8 +1,11 @@
-{wortel, ...}: {
+{wortel, ...}: let
+  username = "wortelworm";
+  homedir = "/home/${username}";
+in {
   # Home Manager needs a bit of information about you
   # and the paths it should manage.
-  home.username = "wortelworm";
-  home.homeDirectory = "/home/wortelworm";
+  home.username = username;
+  home.homeDirectory = homedir;
 
   imports = [
     ./programming
@@ -21,20 +24,39 @@
     DOTNET_CLI_TELEMETRY_OPTOUT = "1";
   };
 
-  # Default programs
-  xdg.mimeApps = {
+  # Cross-Desktop Group, standard specifications from freedesktop.org
+  xdg = {
     enable = true;
 
-    # Use 'xdg-mime query filetype [FILE]' to find out the name
-    defaultApplications = {
-      "text/html" = "firefox.desktop";
-      "x-scheme-handler/http" = "firefox.desktop";
-      "x-scheme-handler/https" = "firefox.desktop";
+    # configuration files
+    configHome = "${homedir}/.xdg-home/config";
 
-      "application/pdf" = "org.pwmt.zathura.desktop";
-      "image/png" = "org.kde.gwenview.desktop";
-      "image/jpeg" = "org.kde.gwenview.desktop";
-      "image/webp" = "org.kde.gwenview.desktop";
+    # data files that are somewhat important, can be synced
+    dataHome = "${homedir}/.xdg-home/data";
+
+    # state like logs and last window state
+    stateHome = "${homedir}/.xdg-home/state";
+
+    # non-essential (cached) data
+    cacheHome = "${homedir}/.xdg-home/cache";
+
+    # Note: runtime dir is automaticly set
+
+    # Default programs
+    mimeApps = {
+      enable = true;
+
+      # Use 'xdg-mime query filetype [FILE]' to find out the name
+      defaultApplications = {
+        "text/html" = "firefox.desktop";
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+
+        "application/pdf" = "org.pwmt.zathura.desktop";
+        "image/png" = "org.kde.gwenview.desktop";
+        "image/jpeg" = "org.kde.gwenview.desktop";
+        "image/webp" = "org.kde.gwenview.desktop";
+      };
     };
   };
 
