5c79a1981618e324f01f70b3bace37cf378b2303
wortelworm <wortelworm@gmail.com>
2025-04-19 13:28:43 +0200

    Libvirtd hooks finished hopefully - wortelworm5 196

--- modules/nixos/virtualization.nix
+++ modules/nixos/virtualization.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   # For future me, if you messed up everything like just now:
   #     Boot from an usb stick (press escape during pre-boot)
   #     Use `sudo mount /dev/nvme0n1p6 /mnt` and then `sudo nixos-enter`
@@ -44,69 +48,78 @@
   #     ls /sys/kernel/iommu_groups/*/devices/* | each {|f| let p = $f.name | parse '/sys/kernel/iommu_groups/{i}/devices/{s}' | get 0; { group: ($p.i | into int), name: (lspci -nns $p.s) }} | sort
   #   Unable to start booting? A program (like btop or cosmic!) is using the graphics card, even though nvidia-smi says otherwise
   #   Note that something like looking glass is not possible, because there is no display output from the dGPU directly...
-  boot.kernelParams = [
-    "amd_iommu=on"
-    "iommu=pt"
-  ];
+  #   Also right now I don't even need it
+  # boot.kernelParams = [
+  #   "amd_iommu=on"
+  #   "iommu=pt"
+  # ];
 
   virtualisation.libvirtd.hooks.qemu = let
     pci_video = "pci_0000_01_00_0";
   in {
     "hardware_isolation" =
-      pkgs.writeScript "hardware_isolation.nu"
-      ''
-        #! ${pkgs.nushell}
-        def main [_guest_name: string, hook_name: string, state_name: string, ..._misc] {
-          if ($hook_name == 'prepare' and $state_name == 'begin') {
+      lib.getExe (pkgs.writeShellApplication {
+        name = "hardware_isolation";
+        # runtimeInputs = with pkgs; [
+        #   # Required for virsh
+        #   libvirt
+        # ];
+        text = ''
+          function enable () {
+            # ## Disable nvidia modules
+            # modprobe -r nvidia_drm
+            # modprobe -r nvidia_modeset
+            # modprobe -r nvidia_uvm
+            # modprobe -r nvidia
+
+            # ## Load vfio
+            # modprobe vfio
+            # modprobe vfio_iommu_type1
+            # modprobe vfio_pci
+
+            # ## Unbind gpu from nvidia and bind to vfio
+            # virsh nodedev-detach ${pci_video}
+
+            ## Limit cores for host
+            systemctl set-property --runtime -- user.slice AllowedCPUs=0,1,2,3
+            systemctl set-property --runtime -- system.slice AllowedCPUs=0,1,2,3
+            systemctl set-property --runtime -- init.scope AllowedCPUs=0,1,2,3
+          }
+
+          function disable () {
+            ## Give host all cores back
+            systemctl set-property --runtime -- init.scope AllowedCPUs=0,1,2,3,4,5,6,7,8,9,11,12,13,14,15
+            systemctl set-property --runtime -- system.slice AllowedCPUs=0,1,2,3,4,5,6,7,8,9,11,12,13,14,15
+            systemctl set-property --runtime -- user.slice AllowedCPUs=0,1,2,3,4,5,6,7,8,9,11,12,13,14,15
+
+            # ## Unbind gpu from vfio
+            # virsh nodedev-reattach ${pci_video}
+
+            # ## Unload vfio
+            # modprobe -r vfio_pci
+            # modprobe -r vfio_iommu_type1
+            # modprobe -r vfio
+
+            # ## Re-enable nvidia modules
+            # modprobe nvidia
+            # modprobe nvidia_uvm
+            # modprobe nvidia_modeset
+            # modprobe nvidia_drm
+          }
+
+          # GUEST_NAME="$1"
+          HOOK_NAME="$2"
+          STATE_NAME="$3"
+
+          if [ "$HOOK_NAME" == "prepare" ] && [ "$STATE_NAME" == "begin" ]; then
             enable
-          }
-          if ($hook_name == 'release' and $state_name == 'end') {
+          fi
+
+          if [ "$HOOK_NAME" == "release" ] && [ "$STATE_NAME" == "end" ]; then
             disable
-          }
-        }
-
-        def enable [] {
-          ## Disable nvidia modules
-          modprobe -r nvidia_drm
-          modprobe -r nvidia_modeset
-          modprobe -r nvidia_uvm
-          modprobe -r nvidia
-
-          ## Load vfio
-          modprobe vfio
-          modprobe vfio_iommu_type1
-          modprobe vfio_pci
-
-          ## Unbind gpu from nvidia and bind to vfio
-          virsh nodedev-detach ${pci_video}
-
-          ## Limit cores for host
-          systemctl set-property --runtime -- user.slice AllowedCPUs=2,3
-          systemctl set-property --runtime -- system.slice AllowedCPUs=2,3
-          systemctl set-property --runtime -- init.scope AllowedCPUs=2,3
-        }
-
-        def disable [] {
-          ## Give host all cores back
-          systemctl set-property --runtime -- user.slice AllowedCPUs=0,1,2,3,4,5,6,7,8,9,11,12,13,14,15
-          systemctl set-property --runtime -- system.slice AllowedCPUs=0,1,2,3,4,5,6,7,8,9,11,12,13,14,15
-          systemctl set-property --runtime -- init.scope AllowedCPUs=0,1,2,3,4,5,6,7,8,9,11,12,13,14,15
-
-          ## Unbind gpu from vfio
-          virsh nodedev-reattach ${pci_video}
-
-          ## Unload vfio
-          modprobe -r vfio
-          modprobe -r vfio_iommu_type1
-          modprobe -r vfio_pci
-
-          ## Re-enable nvidia modules
-          modprobe nvidia_drm
-          modprobe nvidia_modeset
-          modprobe nvidia_uvm
-          modprobe nvidia
-        }
-      '';
+          fi
+        '';
+      });
   };
 
   # Used for building redox
