7441282cb8632c1081d514fb8d173e9a56946dc9
wortelworm <wortelworm@gmail.com>
2025-05-09 20:54:09 +0200

    Airshipper unstable version - wortelworm5 202

--- modules/home-manager/games.nix
+++ modules/home-manager/games.nix
@@ -1,14 +1,13 @@
 {
   lib,
   pkgs,
+  pkgs-unstable,
   wortel,
   ...
 }: {
   home.packages = lib.mkIf wortel.games (with pkgs; [
     prismlauncher
-    # plasma 6 breaks the ui...
-    # Idk but on cosmic does work
-    airshipper
+    pkgs-unstable.airshipper
 
     # Steam is added under nixos, because it needs extra stuff
   ]);
