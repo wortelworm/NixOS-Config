810e37fe3e40211936d6e3a17dd9d74cfa17875c
wortelworm <wortelworm@gmail.com>
2025-07-28 14:33:57 +0200

    Kanata layout experimentation - wortelworm5 230

--- modules/nixos/kanata.nix
+++ modules/nixos/kanata.nix
@@ -1,19 +1,28 @@
-{config, lib, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   services.kanata = lib.mkIf config.wortel.kanata.enable {
     enable = true;
-    keyboards."regulars" = {
-      devices = [
-        "/dev/input/by-id/usb-Logitech_USB_Receiver-if02-event-kbd"
-        "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
-      ];
 
-      # TODO: create navigation layer. See also:
-      #   https://colemakmods.github.io/ergonomic-mods/extend.html
-      #   https://www.reddit.com/r/Colemak/comments/i5vrys/does_anybody_use_extend_layer_for_programming/
+    # Intercept all devices with this config
+    keyboards."regulars" = {
+      # TODO: I'm not entirely happy with this just yet...
+
+      # For an overview of features, see:
+      # https://jtroo.github.io/config.html
+      #
+      # Note that by pressing and holding Left Control, Space, Escape, kanata will exit
+      # Restart using `systemctl start kanata-[keyboard name].service`
+      extraDefCfg = ''
+        process-unmapped-keys yes
+      '';
 
       config = ''
         (defalias
             tem (tap-hold-press 200 200 esc lmet)
+            nav (layer-while-held navigation)
 
             qrt (layer-switch  qwerty)
             col (layer-switch colemak)
@@ -37,13 +46,34 @@
           lctl lmet lalt           spc            ralt rmet cmp  @col
         )
 
+        (defalias
+            aa (tap-hold 200 200 a lmet)
+            rr (tap-hold 200 200 r lalt)
+            ss (tap-hold 200 200 s lsft)
+            tt (tap-hold 200 200 t lctl)
+
+            nn (tap-hold 200 200 n rctl)
+            ee (tap-hold 200 200 e rsft)
+            ii (tap-hold 200 200 i ralt)
+            oo (tap-hold 200 200 o rmet)
+        )
+
         (deflayer colemak
           caps
           grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
           tab  q    w    f    p    g    j    l    u    y    ;    [    ]    \
-          @tem a    r    s    t    d    h    n    e    i    o    '    ret
+          esc  @aa  @rr  @ss  @tt  d    h    @nn  @ee  @ii  @oo  '    ret
           lsft z    x    c    v    b    k    m    ,    .    /    rsft
-          lctl lmet lalt           spc            ralt rmet cmp  @qrt
+          lctl lmet @nav           spc            ralt rmet cmp  @qrt
+        )
+
+        (deflayer navigation
+          _
+          _    _    _    _    _    _    _    _    _    _    _    _    _    _
+          _    _    _    _    _    _    _    _    _    _    _    _    _    _
+          _    lmet lalt lsft lctl _    left down up  right _    _    _
+          _    _    _    _    _    _    _    _    _    _    _    _
+          _    _    _              _              _    _    _    _
         )
       '';
     };
