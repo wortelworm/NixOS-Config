eed660406f52ce08e3383bc50b0361fa26d804e4
wortelworm <wortelworm@gmail.com>
2025-07-30 18:37:07 +0200

    Made Immich actually work - wortelworm5 233

--- modules/nixos/self-hosting/immich.nix
+++ modules/nixos/self-hosting/immich.nix
@@ -6,12 +6,29 @@
   services.immich = lib.mkIf config.wortel.self-hosting.immich {
     enable = true;
     port = 2283;
+
+    # Listen everywhere, allow other devices to access this service
+    host = "0.0.0.0";
     openFirewall = true;
 
     # Disabling any additional features for now, to try it out
     machine-learning.enable = false;
 
-    # openFirewall = true;
-    # Any more settings/users/...??
+    settings = {
+      # Actually informing immich that machine learning is disabled
+      machineLearning.enabled = false;
+
+      storageTemplate = {
+        enabled = true;
+        template = "{{y}}/{{MM}}/{{filename}}";
+      };
+
+      # Altough it updates every once in a while automatically, I would like to know whenever new versions come around
+      newVersionCheck.enabled = true;
+
+      # TODO: Any more settings/...??
+    };
+
+    # Note that users are set up using the UI.
   };
 }
